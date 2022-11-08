5022cf5f99ea82849095f12a15fa03da04f4a84a
Milena Elen <elenprojects7@gmail.com>
2022-11-08 14:45:17 -0300

    feat: add ValueObject Price In Room and config Db

---
+++ BookingService/Adapters/Data/RoomConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Entities = Domain.Entities;
+
+
+namespace Data
+{
+    internal class RoomConfiguration : IEntityTypeConfiguration<Entities.Room>
+    {
+        public void Configure(EntityTypeBuilder<Entities.Room> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.Price).Property(x => x.Currency);
+            builder.OwnsOne(x => x.Price).Property(x => x.Value);
+        }
+    }
+}
