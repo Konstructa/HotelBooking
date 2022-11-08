fd66ca219acd07fe73b01b4404c112b31448feff
Milena Elen <elenprojects7@gmail.com>
2022-11-08 14:44:51 -0300

    feat: add ValueObject personId In Guest and config Db

--- BookingService/Core/Domain/Entities/Guest.cs
+++ BookingService/Core/Domain/Entities/Guest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.ValueObjects;
+
 
 namespace Domain.Entities
 {
@@ -12,5 +9,6 @@ namespace Domain.Entities
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }   
+        public PersonId DocumentId { get; set; }
     }
 }
