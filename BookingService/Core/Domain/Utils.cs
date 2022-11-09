e17830cc53024ecc78e64b04af51bf2ef1c6fe2d
Milena Elen <elenprojects7@gmail.com>
2022-11-09 13:27:46 -0300

    FEAT: create Utils

---
+++ BookingService/Core/Domain/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain
+{
+    public static class Utils
+    {
+        public static bool ValidateEmail(string email)
+        {
+            Regex regex = new (@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+           
+            Match match = regex.Match(email);
+
+            return match.Success;
+        }
+    }
+}
