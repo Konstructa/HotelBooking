444afc4e52fb9fea7cf2a66e6c2cf54924b9754a
Milena Elen <elenprojects7@gmail.com>
2022-11-09 13:27:16 -0300

    FEAT: add Save and Validate

--- BookingService/Core/Domain/Entities/Guest.cs
+++ BookingService/Core/Domain/Entities/Guest.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.ValueObjects;
-
+using System.Reflection.Metadata;
+using Domain.Exceptions;
+using Domain.Ports;
 
 namespace Domain.Entities
 {
@@ -10,5 +12,37 @@ namespace Domain.Entities
         public string Surname { get; set; }
         public string Email { get; set; }   
         public PersonId DocumentId { get; set; }
+
+        private void ValidateState()
+        {
+            if (DocumentId == null ||
+                DocumentId.IdNumber.Length <= 3 ||
+                DocumentId.DocumentType == 0)
+            {
+                throw new InvalidPersonDocumentIdException();
+            }
+            if (Name == null || Surname == null || Email == null)
+            {
+                throw new MissingRequiredInformation();
+            }
+            if(Utils.ValidateEmail(this.Email) == false)
+            {
+                throw new InvalidEmailException();
+            }
+        }
+
+        public async Task Save(IGuestRepository guestRepository)
+        {
+            this.ValidateState();
+
+            if (this.Id == 0)
+            {
+                this.Id = await guestRepository.Create(this);
+            }
+            else
+            {
+
+            }
+        }
     }
 }
