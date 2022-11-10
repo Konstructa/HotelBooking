f87c3dbfc6cc01cc8af4bb54b591e60f0c5bcf10
Milena Elen <elenprojects7@gmail.com>
2022-11-10 13:04:50 -0300

    FEAT: BookingManager

---
+++ BookingService/Core/Application/Booking/BookingManager.cs
@@ -0,0 +1,95 @@
+﻿using Application.Booking.Dtos;
+using Application.Booking.Ports;
+using Application.Responses;
+using Domain.Ports;
+using Domain.Exceptions;
+
+
+namespace Application.Booking
+{
+    public class BookingManager : IBookingManager
+    {
+
+        private readonly IBookingRepository _bookingRepository;
+
+        public BookingManager(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public async Task<BookingResponse> CreateBooking(BookingDto bookingDto)
+        {
+            try
+            {
+                var booking = BookingDto.MapToEntity(bookingDto);
+
+                await booking.Save(_bookingRepository);
+
+                return new BookingResponse
+                {
+                    Success = true,
+                    Data = bookingDto,
+                };
+            }
+            catch (PlacedAtIsARequiredInformationException)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_MISSING_REQUIRED_INFORMATION,
+                    Message = "PlacedAt is a required information"
+                };
+            }
+            catch (StartDateTimeIsRequiredException)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_MISSING_REQUIRED_INFORMATION,
+                    Message = "Start is a required information"
+                };
+            }
+            catch (RoomIsRequiredException)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_MISSING_REQUIRED_INFORMATION,
+                    Message = "Room is a required information"
+                };
+            }
+            catch (GuestIsRequiredException)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_MISSING_REQUIRED_INFORMATION,
+                    Message = "Guest is a required information"
+                };
+            }
+            catch (RoomCannotBeBookedException)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_ROOM_CANNOT_BE_BOOKED,
+                    Message = "The selected Room is not available"
+                };
+            }
+            catch (Exception)
+            {
+                return new BookingResponse
+                {
+                    Success = false,
+                    ErrorCode = ErrorCodes.BOOKING_COULD_NOT_STORE_DATA,
+                    Message = "There was an error when saving to DB"
+                };
+            }
+        }
+
+        public Task<BookingDto> GetBooking(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
