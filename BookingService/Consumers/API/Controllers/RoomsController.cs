1f48e9e0ee4c68f76d80374dce144d98119ffd4e
Milena Elen <elenprojects7@gmail.com>
2022-11-10 11:57:37 -0300

    FEAT: create Room and Booking controller

---
+++ BookingService/Consumers/API/Controllers/RoomsController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Responses;
+using Application.Room.DTO;
+using Application.Room.Ports;
+using Application.Room.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly ILogger<RoomsController> _logger;
+        private readonly IRoomManager _roomManager;
+
+        public RoomsController(ILogger<RoomsController> logger, IRoomManager roomManager)
+        {
+            _logger = logger;
+            _roomManager = roomManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoomDto>> Post(RoomDto room)
+        {
+            var request = new CreateRoomRequest
+            {
+                Data = room
+            };
+
+            var res = await _roomManager.CreateRoom(request);
+
+            if (res.Success) return Created("", res.Data);
+
+            else if (res.ErrorCode == ErrorCodes.ROOM_MISSING_REQUIRED_INFORMATION)
+            {
+                return BadRequest(res);
+            }
+            else if (res.ErrorCode == ErrorCodes.ROOM_COULD_NOT_STORE_DATA)
+            {
+                return BadRequest(res);
+            }
+
+            _logger.LogError("Response with unknown ErrorCode Returned", res);
+            return BadRequest(500);
+        }
+    }
+}
