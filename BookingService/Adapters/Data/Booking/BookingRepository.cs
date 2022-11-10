b17e7fa899419004b39a9b0090f257edc84b5aa5
Milena Elen <elenprojects7@gmail.com>
2022-11-10 11:56:22 -0300

    FEAT: create Booking repository

---
+++ BookingService/Adapters/Data/Booking/BookingRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Domain.Ports;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Booking
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly HotelDbContext _hotelDbContext;
+        public BookingRepository(HotelDbContext hotelDbContext)
+        {
+            _hotelDbContext = hotelDbContext;
+        }
+        public async Task<Domain.Entities.Booking> CreateBooking(Domain.Entities.Booking booking)
+        {
+            _hotelDbContext.Bookings.Add(booking);
+            await _hotelDbContext.SaveChangesAsync();
+            return booking;
+        }
+
+        public async Task<Domain.Entities.Booking> Get(int id)
+        {
+           return await _hotelDbContext.Bookings.Where(x => x.Id == id).FirstAsync();
+        }
+    }
+}
