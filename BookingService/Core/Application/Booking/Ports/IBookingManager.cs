622c97fc6a4666150615e070bb090dc1eef11fdd
Milena Elen <elenprojects7@gmail.com>
2022-11-10 13:04:21 -0300

    FEAT: port Bookmanager

---
+++ BookingService/Core/Application/Booking/Ports/IBookingManager.cs
@@ -0,0 +1,11 @@
+﻿
+using Application.Booking.Dtos;
+
+namespace Application.Booking.Ports
+{
+    public interface IBookingManager
+    {
+        Task<BookingResponse> CreateBooking(BookingDto booking);
+        Task<BookingDto> GetBooking(int id);
+    }
+}
